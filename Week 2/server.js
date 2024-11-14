73683b53f3b60b05146da3ff0c18cb7a5f8dee06
Sonny Yan <s224873117@deakin.edu.au>
2024-11-14 19:44:17 +1100

    Updated with Comment

--- Week 2/server.js
+++ Week 2/server.js
@@ -2,9 +2,10 @@ var express = require("express")
 var app = express()
 app.use(express.static(__dirname+'/public'))
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 var port = process.env.port || 3000;
 
+//Add Function
 const addTwoNumber= (n1,n2) => {
     return n1+n2;
 }
@@ -15,10 +16,10 @@ app.get("/addTwoNumber", (req,res)=>{
     res.json({statuscocde:200, data: result }); 
 });
 
+//Minus Function
 const minusTwoNumber= (n1,n2) => {
     return n1-n2;
 }
-
 app.get("/minusTwoNumber", (req,res)=>{
     const n1= parseInt(req.query.n1);
     const n2=parseInt(req.query.n2);
@@ -26,10 +27,10 @@ app.get("/minusTwoNumber", (req,res)=>{
     res.json({statuscocde:200, data: result }); 
 });
 
+//Multiply Function
 const multiplyTwoNumber= (n1,n2) => {
     return n1*n2;
 }
-
 app.get("/multiplyTwoNumber", (req,res)=>{
     const n1= parseInt(req.query.n1);
     const n2=parseInt(req.query.n2);
@@ -37,15 +38,20 @@ app.get("/multiplyTwoNumber", (req,res)=>{
     res.json({statuscocde:200, data: result }); 
 });
 
+//Divide Function
 const divideTwoNumber= (n1,n2) => {
     return n1/n2;
 }
-
 app.get("/divideTwoNumber", (req,res)=>{
     const n1= parseInt(req.query.n1);
     const n2=parseInt(req.query.n2);
     const result = divideTwoNumber(n1,n2);
     res.json({statuscocde:200, data: result }); 
+});
+
+//404 Not Found
+app.get("/:universalURL", (req, res) => {
+    res.send("404 URL NOT FOUND");
 });
 
 app.listen(port,()=>{
