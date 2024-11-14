daa0c66ea58c95a7955751356ed0fe994542cdea
Sonny Yan <s224873117@deakin.edu.au>
2024-11-14 19:15:30 +1100

    OnTrack Task 2.2P

--- Week 2/server.js
+++ Week 2/server.js
@@ -4,6 +4,7 @@ app.use(express.static(__dirname+'/public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 var port = process.env.port || 3000;
+
 const addTwoNumber= (n1,n2) => {
     return n1+n2;
 }
@@ -11,6 +12,39 @@ app.get("/addTwoNumber", (req,res)=>{
     const n1= parseInt(req.query.n1);
     const n2=parseInt(req.query.n2);
     const result = addTwoNumber(n1,n2);
+    res.json({statuscocde:200, data: result }); 
+});
+
+const minusTwoNumber= (n1,n2) => {
+    return n1-n2;
+}
+
+app.get("/minusTwoNumber", (req,res)=>{
+    const n1= parseInt(req.query.n1);
+    const n2=parseInt(req.query.n2);
+    const result = minusTwoNumber(n1,n2);
+    res.json({statuscocde:200, data: result }); 
+});
+
+const multiplyTwoNumber= (n1,n2) => {
+    return n1*n2;
+}
+
+app.get("/multiplyTwoNumber", (req,res)=>{
+    const n1= parseInt(req.query.n1);
+    const n2=parseInt(req.query.n2);
+    const result = multiplyTwoNumber(n1,n2);
+    res.json({statuscocde:200, data: result }); 
+});
+
+const divideTwoNumber= (n1,n2) => {
+    return n1/n2;
+}
+
+app.get("/divideTwoNumber", (req,res)=>{
+    const n1= parseInt(req.query.n1);
+    const n2=parseInt(req.query.n2);
+    const result = divideTwoNumber(n1,n2);
     res.json({statuscocde:200, data: result }); 
 });
 
