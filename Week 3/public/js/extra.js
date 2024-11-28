36b9cf9be43b59eb839cf51121b51722cf6e5741
Sonny Yan <s224873117@deakin.edu.au>
2024-11-28 19:58:02 +1100

    Adding jQuery JS file

--- Week 3/public/js/extra.js
+++ Week 3/public/js/extra.js
@@ -1,10 +1,19 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     var elems = document.querySelectorAll('.modal');
-//     var instances = M.Modal.init(elems, options);
-//   });
-
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, options);
+  });
+      
   // Or with jQuery
 
   $(document).ready(function(){
+
     $('.modal').modal();
+
+    $("btn1").click(function(){
+      $('this').css('background-color', 'red');
+    });
+
   });
+  
+
+  
