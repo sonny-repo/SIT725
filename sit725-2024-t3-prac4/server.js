b9efae8c4f98d4ded6d8ff12d463cc9826bf6f67
Sonny Yan <s224873117@deakin.edu.au>
2024-12-05 11:25:20 +1100

    Updated images, hhtml, js

--- sit725-2024-t3-prac4/server.js
+++ sit725-2024-t3-prac4/server.js
@@ -1,64 +1,29 @@
-const { response } = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
- 
-// Replace the placeholder with your Atlas connection string
-const uri = "mongodb://localhost:27017/";
- 
-async function insertData() {
-    const client = new MongoClient(uri);
- 
-    try {
-        // Connect to the MongoDB server
-        await client.connect();
- 
-        // Access the database and collection
-        const db = client.db("myDB");
-        const collection = db.collection("pizzaMenu");
- 
-        const pizzas = [
-            { name: "Margherita", ingredients: ["tomato", "mozzarella", "basil"], price: 8.99 },
-            { name: "Pepperoni", ingredients: ["tomato", "mozzarella", "pepperoni"], price: 9.99 },
-            { name: "Veggie Supreme", ingredients: ["tomato", "bell peppers", "onion", "olives"], price: 10.49 }
-        ];
-       
- 
-        // Insert the data
-        const result = await collection.insertMany(pizzas);
-        console.log(`${result.insertedCount} documents inserted:`, result.insertedIds);
-    } catch (err) {
-        console.error('Error inserting data:', err);
-    } finally {
-        // Close the connection
-        await client.close();
-    }
-}
- 
- 
-async function fetchAllData() {
-    const client = new MongoClient(uri);
- 
-    try {
-        // Connect to the MongoDB server
-        await client.connect();
- 
-        // Access the database and collection
-        const db = client.db("myDB");
-        const collection = db.collection("pizzaMenu");
- 
-        // Fetch all documents from the collection
-        const data = await collection.find().toArray();
- 
-        // Print the data
-        console.log(data);
-    } catch (err) {
-        console.error('Error fetching data:', err);
-    } finally {
-        // Close the connection
-        await client.close();
-    }
-}
- 
-// Run the function
-insertData();
-// Run the function
-fetchAllData();
+let express = require('express');
+let app = express()
+
+
+app.use(express.static(__dirname+'/public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Set up the server
+var port = process.env.port || 3000;
+
+
+// // Serve static files from the "public" directory
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// // Route to serve the index.html file
+// app.get('/', function (req, res) {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
+
+//404 Not Found
+app.get("/:universalURL", (req, res) => {
+    res.send("404 URL NOT FOUND");
+});
+
+
+app.listen(port,()=>{
+console.log("App listening to: "+port)
+})
