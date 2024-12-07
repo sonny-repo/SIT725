861ee9edf34a78e93de139663863191dbb9cee22
Sonny Yan <s224873117@deakin.edu.au>
2024-12-07 21:30:12 +1100

    update packages

---
+++ Week 5/routers/routers.js
@@ -0,0 +1,18 @@
+let express = require('express');
+let router = express.Router();
+let controller = require('../controllers/controller');
+
+router.post('/', function(req,res){
+    controller.postItem(req,res);
+});
+
+router.get('/', (req,res)=>{
+    controller.getAllItems(req,res);
+});
+
+router.delete('/', (req,res)=>{
+    controller.getAllItems(req,res);
+});
+
+
+module.exports = router;
