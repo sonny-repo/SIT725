caf0b3f25c97daa783add0957fbdd014c8006fef
Sonny Yan <s224873117@deakin.edu.au>
2024-12-07 21:31:14 +1100

    added database connect js file for MVC

--- Week 5/dbConnect.js
+++ Week 5/dbConnect.js
@@ -1,67 +1,15 @@
-let { response } = require(‘express’);
-let app = expres();
-
-import { MongoClient, ServerApiVersion } from "mongodb";
- 
+const { MongoClient, ServerApiVersion } = require('mongodb');
 // Replace the placeholder with your Atlas connection string
 const uri = "mongodb://localhost:27017/";
 
- 
-async function insertData() {
-    const client = new MongoClient(uri);
- 
-    try {
-        // Connect to the MongoDB server
-        await client.connect();
- 
-        // Access the database and collection
-        const db = client.db("myDB");
-        const collection = db.collection("pizzaMenu");
- 
-        const pizzas = [
-            { name: "Margherita", ingredients: ["tomato", "mozzarella", "basil"], price: 8.99 },
-            { name: "Pepperoni", ingredients: ["tomato", "mozzarella", "pepperoni"], price: 9.99 },
-            { name: "Veggie Supreme", ingredients: ["tomato", "bell peppers", "onion", "olives"], price: 10.49 }
-        ];
-       
- 
-        // Insert the data
-        const result = await collection.insertMany(pizzas);
-        console.log(`${result.insertedCount} documents inserted:`, result.insertedIds);
-    } catch (err) {
-        console.error('Error inserting data:', err);
-    } finally {
-        // Close the connection
-        await client.close();
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
     }
-}
- 
- 
-async function fetchAllData() {
-    const client = new MongoClient(uri);
- 
-    try {
-        // Connect to the MongoDB server
-        await client.connect();
- 
-        // Access the database and collection
-        const db = client.db("myDB");
-        const collection = db.collection("pizzaMenu");
- 
-        // Fetch all documents from the collection
-        const data = await collection.find().toArray();
- 
-        // Print the data
-        console.log(data);
-    } catch (err) {
-        console.error('Error fetching data:', err);
-    } finally {
-        // Close the connection
-        await client.close();
-    }
-}
- 
-// Run the function
-insertData();
-// Run the function
-fetchAllData();
+});
+
+client.connect();
+
+module.exports = client;
