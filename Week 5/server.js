81b394549032fa303121a331358dcabad3041082
Sonny Yan <s224873117@deakin.edu.au>
2024-12-07 21:31:55 +1100

    updating server.js files for MVC

--- Week 5/server.js
+++ Week 5/server.js
@@ -1,85 +1,30 @@
 let express = require('express');
 let app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb://localhost:27017/sit725";
+let port = process.env.port || 3000;
+require('./dbConnection');
+let router = require('./routers/router');
+const { Socket } = require('socket.io');
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
 
-
-app.use(express.static(__dirname+'/public'))
+app.use(express.static(__dirname + '/'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: false}));
+app.use('/api/cat',router);
 
-//Set up the server
-var port = process.env.port || 3000;
+io.on('connection',(socket)=>{
+    console.log('user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 
-
-//Week 4 - Add a Database
-
-//Inserting Data Function 
-async function insertData() {
-    const client = new MongoClient(uri);
- 
-    try {
-        // Connect to the MongoDB server
-        await client.connect();
- 
-        // Access the database and collection
-        const db = client.db("myDB");
-        const collection = db.collection("pizzaMenu");
- 
-        const pizzas = [
-            { name: "Margherita", ingredients: ["tomato", "mozzarella", "basil"], price: 8.99 },
-            { name: "Pepperoni", ingredients: ["tomato", "mozzarella", "pepperoni"], price: 9.99 },
-            { name: "Veggie Supreme", ingredients: ["tomato", "bell peppers", "onion", "olives"], price: 10.49 }
-        ];
-       
- 
-        // Insert the data
-        const result = await collection.insertMany(pizzas);
-        console.log(`${result.insertedCount} documents inserted:`, result.insertedIds);
-    } catch (err) {
-        console.error('Error inserting data:', err);
-    } finally {
-        // Close the connection
-        await client.close();
-    }
-}
- 
-//Pushing Data to myDB
-async function fetchAllData() {
-    const client = new MongoClient(uri);
- 
-    try {
-        // Connect to the MongoDB server
-        await client.connect();
- 
-        // Access the database and collection
-        const db = client.db("myDB");
-        const collection = db.collection("pizzaMenu");
- 
-        // Fetch all documents from the collection
-        const data = await collection.find().toArray();
- 
-        // Print the data
-        console.log(data);
-    } catch (err) {
-        console.error('Error fetching data:', err);
-    } finally {
-        // Close the connection
-        await client.close();
-    }
-}
- 
-// Run the function
-insertData();
-// Run the function
-fetchAllData();
-
-//404 Not Found
-app.get("/:universalURL", (req, res) => {
-    res.send("404 URL NOT FOUND");
+    setInterval(()=>{
+        x=parseInt(Math.random()*10);
+        socket.emit('number', x);
+        console.log('Emmiting Number '+x);
+    }, 1000)
 });
 
-
-app.listen(port,()=>{
-console.log("App listening to: "+port)
-})
+http.listen(port, ()=>{
+    console.log('express server started');
+});
