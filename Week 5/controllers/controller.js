35bbe668cb68c36050dbb8b1818daec97213684c
Sonny Yan <s224873117@deakin.edu.au>
2024-12-07 21:32:14 +1100

    creating controller.js files for MVC

---
+++ Week 5/controllers/controller.js
@@ -0,0 +1,29 @@
+let collection = require('../models/item');
+
+const postItem = (req,res) => {
+    let item = req.body;
+    collection.postCat(cat, (err,result) => {
+        if (!err) {
+            res.json({statusCode:201,data:result,message:'success'});
+        }
+    });
+}
+
+const getAllItems = (req,res) => {
+    collection.getAllItems((error,result)=>{
+        if (!error) {
+            res.json({statusCode:200,data:result,message:'success'});
+        }
+    });
+}
+
+const deleteItem = (req,res) => {
+    let cat = req.body;
+    collection.deleteOne(cat, (err,result) => {
+        if (!err) {
+            res.json({statusCode:201,data:result,message:'success'});
+        }
+    });
+}
+
+module.exports = {postItem, getAllItems}
