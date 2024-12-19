644f51aa8a5abe24483f9ca8af8140b305447a2b
Sonny Yan <s224873117@deakin.edu.au>
2024-12-19 16:27:12 +1100

    week 6 update

--- Week 6/server.js
+++ Week 6/server.js
@@ -38,3 +38,14 @@ io.on('connection',(socket)=>{
 app.get("/:universalURL", (req, res) => {
     res.send("404 URL NOT FOUND");
 });
+
+//Week 6 - Testing
+app.get('/addTwoNumbers/:firstNumber/:secondNumber', function(req,res,next){
+    var firstNumber = parseInt(req.params.firstNumber) 
+    var secondNumber = parseInt(req.params.secondNumber)
+    var result = firstNumber + secondNumber || null
+    if(result == null) {
+      res.json({result: result, statusCode: 400}).status(400)
+    }
+    else { res.json({result: result, statusCode: 200}).status(200) } 
+  })
